657310857d627f8a4de79fc56be33b207017dd2a
sumitusf <sumit141@usf.edu>
2025-09-09 22:07:19 -0400

    Create programv2.cs

---
+++ programv2.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        int maxHeartRate = 220 - age;
+        Console.WriteLine($"Your estimated Maximum Heart Rate is: {maxHeartRate} bpm");
+    }
+}
